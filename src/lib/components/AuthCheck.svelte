529e8fd6ee58016e90279cec2c9aecc815088e2c
mhadziqrazin <mhadziq0403@gmail.com>
2023-07-30 20:03:16 +0700

    feat: auth checker

---
+++ src/lib/components/AuthCheck.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { user } from '$lib/firebase'
+</script>
+
+{#if $user}
+  <slot />
+{:else}
+  <p class="text-error">
+    You must be signed in to view this page.
+    <a class="text-purple-500 underline hover:no-underline hover:text-purple-300" href="/login">Sign in</a>
+  </p>
+{/if}
