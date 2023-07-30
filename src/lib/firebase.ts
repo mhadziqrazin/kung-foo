061234718fbfd96a96de084936ecc9ebe8cffb62
mhadziqrazin <mhadziq0403@gmail.com>
2023-07-30 13:30:28 +0700

    feat: auth store

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { getApps, initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
-import { getAuth } from 'firebase/auth'
+import { getAuth, onAuthStateChanged, type User } from 'firebase/auth'
 import { getStorage } from 'firebase/storage'
+import { writable } from 'svelte/store'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -22,3 +23,28 @@ if (!getApps.length) {
 export const db = getFirestore()
 export const auth = getAuth()
 export const storage = getStorage()
+
+function userStore() {
+  let unsubscribe: () => void
+  if (!auth || !globalThis.window) {
+    console.warn('Auth is not initialized or not in browser')
+    const { subscribe } = writable<User | null>(null)
+    return {
+      subscribe
+    }
+  }
+
+  const { subscribe } = writable(auth.currentUser ?? null, (set) => {
+    unsubscribe = onAuthStateChanged(auth, (user) => {
+      set(user)
+    })
+
+    return () => unsubscribe()
+  })
+
+  return {
+    subscribe
+  }
+}
+
+export const user = userStore()
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { auth } from '$lib/firebase'
+  import { auth, user } from '$lib/firebase'
   import { signInWithPopup, GoogleAuthProvider, signOut } from 'firebase/auth'
 
   const handleSignIn = async () => {
@@ -10,10 +10,17 @@
 </script>
 
 <div class="flex flex-col items-center gap-10">
-  <h1>
-    {'Login'}
-  </h1>
-  <button class="btn btn-primary" on:click={handleSignIn}>
-    {'Sign in with Google'}
-  </button>
+  <h1>Login</h1>
+
+  {#if $user}
+    <div class="flex flex-col items-center">
+      <p>
+        Hi, {$user.displayName}!
+      </p>
+      <p class="text-green-500">You are logged in</p>
+    </div>
+    <button class="btn btn-warning" on:click={() => signOut(auth)}> Sign out </button>
+  {:else}
+    <button class="btn btn-primary" on:click={handleSignIn}> Sign in with Google </button>
+  {/if}
 </div>
