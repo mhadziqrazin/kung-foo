c5f4814d22f0ca6ed3e765e5f62364b04a50e061
mhadziqrazin <mhadziq0403@gmail.com>
2023-07-30 22:01:35 +0700

    style: check username availability text style

--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import { db } from '$lib/firebase'
+  import { db, user } from '$lib/firebase'
   import AuthCheck from '$lib/components/AuthCheck.svelte'
-  import { doc, getDoc } from 'firebase/firestore'
+  import { doc, getDoc, writeBatch } from 'firebase/firestore'
 
   let username = ''
   let loading = false
@@ -12,11 +12,16 @@
     isAvailable = true
     loading = true
 
+    if (username === '') {
+      loading = false
+      isAvailable = false
+      return
+    }
+
     clearTimeout(debounceTimer)
     debounceTimer = setTimeout(async () => {
       const ref = doc(db, 'usernames', username)
       const exists = await getDoc(ref).then((doc) => doc.exists())
-
       isAvailable = !exists
       loading = false
     }, 500)
@@ -48,10 +53,12 @@
     <h1>Username</h1>
     <form on:submit|preventDefault={confirmUsername} class="flex flex-col items-center gap-4">
       <input type="text" placeholder="Username" bind:value={username} on:input={checkAvailability} class="input" />
-      {#if isAvailable}
-        <p>Username available!</p>
+      {#if isAvailable && !loading}
+        <p class="text-green-500">Username available!</p>
+      {:else if !isAvailable && !loading}
+        <p class="text-red-500">Username not available! Try a different one.</p>
       {:else}
-        <p>Username not available! Try a different one.</p>
+        <p>Loading..</p>
       {/if}
       <button class="btn btn-success"> Confirm username @{username} </button>
     </form>
