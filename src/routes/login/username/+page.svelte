bdee5b47858354ba1ca74777433883bf85aaa083
mhadziqrazin <mhadziq0403@gmail.com>
2023-07-30 22:01:17 +0700

    feat: confirm username

--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -22,7 +22,25 @@
     }, 500)
   }
 
-  async function confirmUsername() {}
+  async function confirmUsername() {
+    const batch = writeBatch(db)
+    batch.set(doc(db, 'usernames', username), { uid: $user?.uid })
+    batch.set(doc(db, 'users', $user!.uid), {
+      username,
+      photoURL: $user?.photoURL ?? null,
+      published: true,
+      bio: 'I am the Walrus',
+      links: [
+        {
+          title: 'Test Link',
+          url: 'https://kung.foo',
+          icon: 'custom',
+        },
+      ],
+    })
+
+    await batch.commit()
+  }
 </script>
 
 <AuthCheck>
